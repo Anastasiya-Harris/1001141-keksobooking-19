470f38f081763008de19c311ac1eef8ff94f0159
Anastasiya-Harris <ufadesigner@gmail.com>
2020-02-02 19:53:19 +0500

    Создает массив из объявлений

--- js/main.js
+++ js/main.js
@@ -6,13 +6,12 @@ var CHECKINS = ['12:00', '13:00', '14:00'];
 var CHECKOUTS = ['12:00', '13:00', '14:00'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-var MAX_PRICE =;
-var MIN_PRICE = ;
+var MAX_PRICE = 1000;
+var MIN_PRICE = 100000;
 
-
-var locationX =  "x": случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-var locationY = getRandomNumber(130, 630);
-
+// Показывает карту
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
 
 
 // Функция, возвращающая рандомное число в диапазоне между параметрами min и max.
@@ -21,44 +20,48 @@ var getRandomNumber = function (min, max) {
   return randomNumber;
 };
 
+
 // Функция, возвращающая один рандомный элемент из переданного массива.
 var getRandomFromArray = function () {
-  var randomFromArray = getRandomNumber(0, array.length)??????
+  var randomFromArray = getRandomNumber(0, array.length)
   return array.randomFromArray;
   //var rand = myArray[Math.floor(Math.random() * myArray.length)];
 };
 
+// Функция, для создания массива из 8 сгенерированных объявлений.
+var createAds = function () {
+  var author = {
+    'avatar': 'img/avatars/user0' + (i + 1) + '.png'
+    // "avatar": img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
+  };
 
-var author {
-  'avatar': 'img/avatars/user0' + (i + 1) + '.png'
+  var ads = {
+    'title': 'string',
+    // 'address': строка, адрес предложения. Для простоты пусть пока представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
+    'price': getRandomNumber(MIN_PRICE, MAX_PRICE),
+    'type': getRandomFromArray(TYPE),
+    'rooms': number,
+    'guests': number,
+    'checkin': CHECKINS,
+    'checkout': CHECKOUTS,
+    'features': getRandomFromArray(FUTURES),
+    'description': 'строка с описанием',
+    'photos': getRandomFromArray(PHOTOS)
+  };
+
+    var location = {
+      x: getRandomNumber(0, 1138),
+      y: getRandomNumber(130, 630)
+  };
 };
-"avatar": img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
-        },
 
+// Вставляет шабон на карту
+var map = document.querySelectorAll('.map');
 
-var offer = {
-  'title': 'string',
-  // 'address': строка, адрес предложения. Для простоты пусть пока представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
-  'price':  getRandomNumber(MIN_PRICE, MAX_PRICE),
-  'type':  [palace, flat, house или bungalo], //строка с одним из четырёх фиксированных значений:
-  'rooms': number,
-  'guests': number,
-  'checkin': CHECKINS,
-  'checkout': CHECKOUTS,
-  'features': getRandomFromArray(FUTURES),
-  'description': 'строка с описанием',
-  'photos': getRandomFromArray(PHOTOS)
-  },
+var template = document.querySelector('#card').content.querySelector('.map__card');
 
-    // "location": {
-    // "x": случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-    // "y": случайное число, координата y метки на карте от 130 до 630.
-    // }
-// }
-
-//Напишите функцию для создания массива из 8 сгенерированных JS объектов
-// функцию генерации Случайных данных,
-// функцию создания DOM-элемента на основе JS-объекта,
-// функцию заполнения блока DOM-элементами на основе массива JS-объектов массив
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+for (var i = 0; i < 8; i++) {
+  var element = template.cloneNode(true);
+  element.children[0].textContent = i;
+  map[1].appendChild(element);
+}
